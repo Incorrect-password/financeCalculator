02b5116d5a777275a6ee7c8b6cb44b882da9ba24
James Taylor <taylor-j12@hotmail.com>
2019-10-19 08:35:11 +0100

    tested all the functionality from the previous commit

--- app/js/dist/main.js
+++ app/js/dist/main.js
@@ -4,49 +4,44 @@ var expectedSalary = Number(document.querySelector('#expectedSalary').value);
 var repaymentPercentage = Number(document.querySelector('#repaymentPercentage').value);
 document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
-    // document.querySelector('#borrowed #expectedSalary #repaymentPercentage').addEventListener('input', function () {
-    //     //can you have multiple selectors like this?
-    //     //totalBorrowed(borrowed)
-    // });
+    document.querySelector('#borrowed').addEventListener('input', function () {
+        //can you have multiple selectors like this?
+        totalBorrowed(borrowed);
+    });
 });
-// function totalBorrowed(borrowed:number):number {
-//
-//     if (borrowed < 6400) {
-//         let total: number = borrowed + (borrowed/100)*5
-//         //document.querySelector('#totalBorrowed').innerHTML = String(total)
-//         //document.querySelector('#totalFees').innerHTML = (borrowed/100)*5
-//         // document.querySelector('#payback').innerHTML = payback(total, expectedSalary, repaymentPercentage)
-//         console.log(total)
-//         console.log(String(total))
-//         return total
-//
-//     }else if (borrowed < 7200) {
-//         let fees: number = ((borrowed/100)*5) + 500
-//         let total: number = borrowed + fees
-//         // document.querySelector('#totalFees').innerHTML = fees
-//         //document.querySelector('#totalBorrowed').innerHTML = String(total)
-//         // document.querySelector('#payback').innerHTML = payback(total, expectedSalary, repaymentPercentage)
-//
-//
-//
-//     }else if (borrowed < 8000) {
-//         let fees: number = ((borrowed/100)*5) + 1000
-//         let total: number = borrowed + ((borrowed/100)*5) + 1000
-//         // document.querySelector('#totalFees').innerHTML = fees
-//         //document.querySelector('#totalBorrowed').innerHTML = String(total)
-//         // document.querySelector('#payback').innerHTML = payback(total, expectedSalary, repaymentPercentage)
-//
-//
-//
-//     }else{
-//         document.querySelector('#borrowed').innerHTML = 'The amount inputted does not match the criteria. Please refresh and start again.'
-//     }
-//
-// }
-// totalBorrowed(borrowed)
+function totalBorrowed(borrowed) {
+    if (borrowed <= 6400) {
+        var total = borrowed + (borrowed / 100) * 5;
+        document.querySelector('#totalBorrowed').innerHTML = '<p>' + total;
+        '</p>';
+        document.querySelector('#totalFees').innerHTML = '<p>' + (borrowed / 100) * 5 + '</p>';
+        document.querySelector('#payback').innerHTML = '<p>' + payback(total, expectedSalary, repaymentPercentage) + '</p>';
+        return total;
+    }
+    else if (borrowed <= 7200) {
+        var fees = ((borrowed / 100) * 5) + 500;
+        var total = borrowed + fees;
+        document.querySelector('#totalBorrowed').innerHTML = '<p>' + total;
+        '</p>';
+        document.querySelector('#totalFees').innerHTML = '<p>' + fees + '</p>';
+        document.querySelector('#payback').innerHTML = '<p>' + payback(total, expectedSalary, repaymentPercentage) + '</p>';
+    }
+    else if (borrowed <= 8000) {
+        var fees = ((borrowed / 100) * 5) + 1000;
+        var total = borrowed + ((borrowed / 100) * 5) + 1000;
+        document.querySelector('#totalBorrowed').innerHTML = '<p>' + total;
+        '</p>';
+        document.querySelector('#totalFees').innerHTML = '<p>' + fees + '</p>';
+        document.querySelector('#payback').innerHTML = '<p>' + payback(total, expectedSalary, repaymentPercentage) + '</p>';
+    }
+    else {
+        document.querySelector('#borrowed').innerHTML = 'The amount inputted does not match the criteria. Please refresh and start again.';
+    }
+}
+totalBorrowed(borrowed);
 function payback(total, expectedSalary, repaymentPercentage) {
     var monthlyPayment = ((expectedSalary / 12) / 100) * repaymentPercentage;
-    var months = (total / monthlyPayment); //round up
+    var months = Math.ceil((total / monthlyPayment));
     return months;
 }
 payback(100, 25000, 10);
