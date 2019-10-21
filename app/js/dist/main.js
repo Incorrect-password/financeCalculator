8d1ec07523e9787e4d32a49f1fb5f0bb1e12174f
James Taylor <taylor-j12@hotmail.com>
2019-10-21 07:42:01 +0100

    got the functionality of the main calculations working, attempted some further js but unable to check without internet, add some css to results display that needs to be checked when internet is available

--- app/js/dist/main.js
+++ app/js/dist/main.js
@@ -1,47 +1,67 @@
-//total amount = borrowed + maybe 500 + maybe 500 - 5%
 var borrowed = Number(document.querySelector('#borrowed').value);
 var expectedSalary = Number(document.querySelector('#expectedSalary').value);
 var repaymentPercentage = Number(document.querySelector('#repaymentPercentage').value);
-document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    document.querySelector('#borrowed').addEventListener('input', function () {
-        //can you have multiple selectors like this?
-        totalBorrowed(borrowed);
+var upFront;
+var total;
+var months;
+everything();
+function everything() {
+    document.querySelector('form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        if (borrowed > 0 && borrowed <= 8000 && repaymentPercentage >= 10 && repaymentPercentage <= 100) {
+            upFront = upFrontCalc(borrowed);
+            total = totalBorrowed(borrowed);
+            months = paybackTime(total, expectedSalary, repaymentPercentage);
+            resultPrint(upFront, total, months);
+        }
+        else {
+            document.querySelector('#resultDisplay').innerHTML = 'read the fucking instructions';
+        }
+        document.querySelector('#borrowed').addEventListener('input', function () {
+            everything();
+        });
+        document.querySelector('#expectedSalary').addEventListener('input', function () {
+            everything();
+        });
+        document.querySelector('#repaymentPercentage').addEventListener('input', function () {
+            everything();
+        });
     });
-});
+}
+function upFrontCalc(borrowed) {
+    upFront = ((borrowed / 100) * 5);
+    return upFront;
+}
 function totalBorrowed(borrowed) {
     if (borrowed <= 6400) {
-        var total = borrowed + (borrowed / 100) * 5;
-        document.querySelector('#totalBorrowed').innerHTML = '<p>' + total;
-        '</p>';
-        document.querySelector('#totalFees').innerHTML = '<p>' + (borrowed / 100) * 5 + '</p>';
-        document.querySelector('#payback').innerHTML = '<p>' + payback(total, expectedSalary, repaymentPercentage) + '</p>';
+        total = borrowed - upFront;
         return total;
     }
     else if (borrowed <= 7200) {
-        var fees = ((borrowed / 100) * 5) + 500;
-        var total = borrowed + fees;
-        document.querySelector('#totalBorrowed').innerHTML = '<p>' + total;
-        '</p>';
-        document.querySelector('#totalFees').innerHTML = '<p>' + fees + '</p>';
-        document.querySelector('#payback').innerHTML = '<p>' + payback(total, expectedSalary, repaymentPercentage) + '</p>';
+        total = borrowed + 500 - upFront;
+        return total;
     }
     else if (borrowed <= 8000) {
-        var fees = ((borrowed / 100) * 5) + 1000;
-        var total = borrowed + ((borrowed / 100) * 5) + 1000;
-        document.querySelector('#totalBorrowed').innerHTML = '<p>' + total;
-        '</p>';
-        document.querySelector('#totalFees').innerHTML = '<p>' + fees + '</p>';
-        document.querySelector('#payback').innerHTML = '<p>' + payback(total, expectedSalary, repaymentPercentage) + '</p>';
+        total = borrowed + 1000 - upFront;
+        return total;
     }
     else {
         document.querySelector('#borrowed').innerHTML = 'The amount inputted does not match the criteria. Please refresh and start again.';
     }
 }
-totalBorrowed(borrowed);
-function payback(total, expectedSalary, repaymentPercentage) {
+function paybackTime(total, expectedSalary, repaymentPercentage) {
     var monthlyPayment = ((expectedSalary / 12) / 100) * repaymentPercentage;
     var months = Math.ceil((total / monthlyPayment));
     return months;
 }
-payback(100, 25000, 10);
+function resultPrint(upFront, total, months) {
+    var source = document.querySelector('#resultsTemplate').innerHTML;
+    var template = Handlebars.compile(source);
+    var data = {
+        fees: upFront,
+        totals: total,
+        month: months
+    };
+    var html = template(data);
+    document.querySelector('#resultDisplay').innerHTML = html;
+}
